cb05a5227aac72bcd495115ffe0ade97e107ba15
Joshua Blanchard <joshua.lee.bbg@gmail.com>
2019-07-16 21:46:36 -0400

    ocean: load touch modules from vendor
    * instead of shipping the touch modules, which
      apparently need to be updated with firmware updates,
      load them directly from vendor
    
    Change-Id: I63fd321d5a3cc9900178ae3af53cc13bd17577e1
    Signed-off-by: Joshua Blanchard <joshua.lee.bbg@gmail.com>

--- recovery/root/sbin/load_drivers.sh
+++ recovery/root/sbin/load_drivers.sh
@@ -1,54 +1,42 @@
 #!/sbin/sh
 
-load_panel_modules()
-{
-    path=$1
-    panel_supplier=""
-    panel_supplier=$(cat /sys/devices/virtual/graphics/fb0/panel_supplier 2> /dev/null)
-
-    case $panel_supplier in
-        csot)
-            insmod $path/nova_mmi.ko
-            ;;
-	ofilm)
-	    insmod $path/focaltech_mmi.ko
-	    ;;
-	tianma)
-	    insmod $path/ilitek_mmi.ko
-	    ;;
-        *)
-            echo "$panel_supplier not supported"
-            ;;
-    esac
-}
-
-# Main
 SLOT=`getprop ro.boot.slot_suffix`
 mount /dev/block/bootdevice/by-name/vendor$SLOT /vendor -o ro
 
+panel_supplier=""
+panel_supplier=$(cat /sys/devices/virtual/graphics/fb0/panel_supplier 2> /dev/null)
+echo "panel supplier vendor is: [$panel_supplier]"
+
+case $panel_supplier in
+	ofilm)
+		insmod /vendor/lib/modules/focaltech_mmi.ko
+		;;
+	tianma)
+		insmod /vendor/lib/modules/ilitek_mmi.ko
+		;;
+	csot)
+		insmod /vendor/lib/modules/nova_mmi.ko
+		;;
+	*)
+		echo "$panel_supplier not supported"
+		;;
+esac
+
 # MMI Common
 insmod /vendor/lib/modules/exfat.ko
-insmod /vendor/lib/modules/utags.ko
-insmod /vendor/lib/modules/sensors_class.ko
 insmod /vendor/lib/modules/mmi_annotate.ko
 insmod /vendor/lib/modules/mmi_info.ko
-insmod /vendor/lib/modules/tzlog_dump.ko
 insmod /vendor/lib/modules/mmi_sys_temp.ko
+insmod /vendor/lib/modules/sensors_class.ko
+insmod /vendor/lib/modules/tzlog_dump.ko
+insmod /vendor/lib/modules/utags.ko
 
-# Ocean specific?
-insmod /vendor/lib/modules/tps61280.ko
+
+# Ocean specific
+insmod /vendor/lib/modules/aw8624.ko
 insmod /vendor/lib/modules/drv2624_mmi.ko
 insmod /vendor/lib/modules/sx932x_sar.ko
-
-# Load panel modules
-if [ -d /vendor/lib/modules ]; then
-    load_panel_modules /vendor/lib/modules
-else
-    # In case /vendor is empty for whatever reason
-    # make sure at least touchscreen is working
-    load_panel_modules /sbin/modules
-fi
+insmod /vendor/lib/modules/tps61280.ko
 
 umount /vendor
 setprop drivers.loaded 1
-
